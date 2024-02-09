b487fdaeacc38b7d393e567d6742f6358316eb55
GopalAror <punjabijanu586@gmail.com>
2024-02-09 15:19:01 +0530

    drop shadow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                         <img className='w-full max-w-[394px] animate-bounce2' src={aeroplanehero} alt="aeroplane" data-aos="zoom-in" />
                     </div>
                     <div className="sm:w-7/12 w-full px-3 flex justify-center" data-aos="zoom-in">
-                        <h1 className="ff-lucky max-w-[398px] text-center font-normal leading-[90%] drop-shadow-[0px_1.337px_0px_#000] text-[#FBA11D] md:text-[175px] text-[80px] sm:text-[130px]">
+                        <h1 className="ff-lucky max-w-[398px] text-center font-normal leading-[90%] drop-shadow-[0px_6px_0px_#000] text-[#FBA11D] md:text-[175px] text-[80px] sm:text-[130px]">
                             D<span className="text-[#23AAAC]">I</span>
                             <span className="text-[#D45B07]">N</span>
                             <span className="text-[#8EC627]">O</span>
